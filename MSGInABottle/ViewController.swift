a76be8afa6ab08a78f5c117f3c75ae40504a3513
Sahil Jain <sahil@sahiljain.ca>
2016-10-01 15:27:51 -0400

    initialize xcode project

---
+++ MSGInABottle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSGInABottle
+//
+//  Created by Sahil Jain on 10/1/16.
+//  Copyright © 2016 Internal Mini-Project Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
