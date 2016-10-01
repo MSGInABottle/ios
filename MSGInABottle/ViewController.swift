a3076eb6fce55fc252dfc6e947fc2b860c9dcd25
Sahil Jain <sahil@sahiljain.ca>
2016-10-01 18:24:13 -0400

    add some ui elements

--- MSGInABottle/ViewController.swift
+++ MSGInABottle/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMessageButton(_ sender: UIButton) {
+        print("BUTTON PRESSED")
+    }
 
 }
-
